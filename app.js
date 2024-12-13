5464dbd6d52a6af4553a1c1596f122cd80bdfa2e
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-12-13 17:12:09 -0300

    reiniar botao

--- app.js
+++ app.js
@@ -7,14 +7,40 @@ function sortear(){
     let numero;
 
     for(let i = 0; i < quantidade; i++){ // Executa um loop 'quantidade' de vezes para sortear os números
-        numero = obterNumeroAleatorio(de, ate)  // Gera um número aleatório no intervalo especificado (entre 'de' e 'ate')
+        numero = obterNumeroAleatorio(de, ate);  // Gera um número aleatório no intervalo especificado (entre 'de' e 'ate')
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
         sorteados.push(numero);  // Adiciona o número sorteado ao array 'sorteados'
     }
 
-    alert(sorteados);
-  
+   let resultado = document.getElementById('resultado');
+   resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+   alterarStatusBotao();
 }
   // Gera um número aleatório entre 'min' e 'max', incluindo ambos os extremos
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    // Declara uma variável 'botao' e atribui a ela o elemento HTML com o ID 'btn-reiniciar'
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarStatusBotao();
 }
